74310f6f581ec395fac247ec087a56203fc4c51a
doraemi2k1 <hoatuyetmuadong02032000@gmail.com>
2020-10-31 14:43:45 +0700

    daycot

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bài_th3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ bài 2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace btth3 // bài 2
+{
+    class Unit
+    {
+        private double x;
+        int n, k;
+
+
+
+
+    }
+    class test1
+    {
+        static void Main2()
+
+    }
+}
